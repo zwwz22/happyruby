7ef724de632115b0204500987b6a285d9cc36a93
zhangw <zwwz22@sina.com>
2014-07-21 11:35:04 +0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happyruby::Application.config.secret_token = 'fd080cb8455eb09c8ae703bb95a7ea019d63150f65b8ca6ae08a1591836fdb072e028aca819fddce11729ce407ba8c8fba879e3f749179139b52108a9ca59a8e'
